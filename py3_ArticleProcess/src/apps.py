711eb10ad7877d5536c9fb96c1e6400b8893ef30
Allen Kuo <yxakuo@hotmail.com>
2017-09-25 19:50:03 +0800

    Add sqlalchemy, pymysql, colorama

---
+++ py3_ArticleProcess/src/apps.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+__author__ = 'yxakuo@hotmail.com'
+
+"""
+@version: _____
+@author: Allen tGuo
+@license: Apache Licence
+@contact: 
+@site: http://www.bizrapid.com
+@software: PyCharm
+@file: apps.py
+@time: 2017-08-14 下午2:07
+"""
+
+import argparse
+
+
+def version():
+    pass
+
+
+def changelog():
+    pass
+
+
+def todo():
+    pass
+
+
+def help():
+    pass
+
+
+def testcase():
+    pass
+
+
+class Main(object):
+    def __init__(self):
+        pass
+
+
+def user_function():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
